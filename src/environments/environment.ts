15c04c3a7198ead12ef8d8a6ee1587696f26dac3
Shreyansh Saagar <shreyansh@tritv65.com>
2026-04-20 01:23:50 +0530

    deployment small issue fix --Shreyansh Saagar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://persona-ai-api-kappa.vercel.app/api/v1/',
+  apiBaseUrl: 'https://persona-ai-api-kappa.vercel.app/api/v1',
 };
